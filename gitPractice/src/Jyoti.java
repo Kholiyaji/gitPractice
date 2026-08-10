26e0572ab105371d504d762b5f42317b085fdfba
Jyoti Palariya <palariyajyoti456@gmail.com>
2026-08-10 20:58:24 +0530

    my first commit

---
+++ gitPractice/src/Jyoti.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Jyoti {
+    public static int findSecondLargest(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return -1;
+        }
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num != largest) {
+                secondLargest = num;
+            }
+        }
+
+        return (secondLargest == Integer.MIN_VALUE) ? -1 : secondLargest;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 20, 8, 20, 15};
+        System.out.println(findSecondLargest(arr));
+    }
+}
